a274269858c4dcd4ee799568914eb51d13f5963c
bhashmi88 <44117443+bhashmi88@users.noreply.github.com>
2023-02-21 10:39:55 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+const signupButton = document.getElementById('signup-button');
+const signupFormContainer = document.getElementById('signup-form-container');
+
+signupButton.addEventListener('click', () => {
+	signupFormContainer.classList.add
